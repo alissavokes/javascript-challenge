ae1655b1ec4a9f270b11e2785bac31d5d721ec7f
alissavokes <alissavokes@gmail.com>
2020-09-06 12:30:11 -0400

    read data.js to add ufo data to table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
+let tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+console.log(data);
+
+//Loop Through `data` and console.log each weather report object
+data.forEach(function(ufoSighting){
+    console.log(ufoSighting);
+    //Use d3 to append one table row `tr` for each weather report object
+    let row = tbody.append("tr");
+    //Use `Object.entries` to console.log each weather report value
+    Object.entries(ufoSighting).forEach(function([key, value]){
+        console.log(key, value);
+        //Use d3 to append 1 cell per ufo sighting value (datetime, city, state, 
+        //country, shape, durationMinutes, comments)
+        let cell = row.append("td");
+        //Use d3 to update each cell's text with ufo sighting value
+        cell.text(value);
+    });
+});
